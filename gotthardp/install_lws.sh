d6f2f51c1ff4fe2cb5bf1b981c603e591a42a9e1
Stéphane David <stephane.david@1-gate.com>
2017-05-28 12:28:06 +0200

    modified:   gotthardp/install_lws.sh

--- gotthardp/install_lws.sh
+++ gotthardp/install_lws.sh
@@ -7,14 +7,13 @@ set -e
 
 # Download and install debian package 
 wget https://github.com/gotthardp/lorawan-server/releases/download/v0.4.2/lorawan-server_0.4.2_all.deb
-tar -zxvf lorawan-server-0.3.0.tar.gz
+dpkg -i lorawan-server_0.4.2_all.deb
 
 echo "Installation completed."
 
 # Start lorawan server as a service
 
 systemctl enable lorawan-server.service
-systemctl start lorawan-server.service
 
 echo "The system will reboot in 5 seconds..."
 sleep 5
