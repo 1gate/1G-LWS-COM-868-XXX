624fb0925817158d36722ac23c9e48c5d909f883
Stéphane David <stephane.david@1-gate.com>
2017-07-01 11:44:49 +0200

    Update install_lws.sh

--- gotthardp/install_lws.sh
+++ gotthardp/install_lws.sh
@@ -6,7 +6,7 @@ echo "Install LoRaWAN Server"
 set -e
 
 # Download and install debian package 
-wget https://github.com/gotthardp/lorawan-server/releases/download/v0.4.4/lorawan-server_0.4.4_all.deb
+wget https://github.com/gotthardp/lorawan-server/releases/download/v0.4.4/lorawan-server_0.4.5_all.deb
 dpkg -i lorawan-server_0.4.4_all.deb
 
 echo "Installation completed."
