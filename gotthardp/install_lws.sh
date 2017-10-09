9e4e5c2518abdc76e8267cba3f30e876916999b8
Stéphane David <stephane.david@1-gate.com>
2017-10-09 13:12:49 +0200

    Update install_lws.sh

--- gotthardp/install_lws.sh
+++ gotthardp/install_lws.sh
@@ -15,8 +15,8 @@ chmod a+rwx $LORAWAN_HOME
 fi
 
 # Download and install debian package 
-wget https://github.com/gotthardp/lorawan-server/releases/download/v0.4.11/lorawan-server_0.4.11_all.deb
-dpkg -i lorawan-server_0.4.11_all.deb
+wget https://github.com/gotthardp/lorawan-server/releases/download/v0.4.12/lorawan-server_0.4.12_all.deb
+dpkg -i lorawan-server_0.4.12_all.deb
 
 echo "Installation completed."
 
