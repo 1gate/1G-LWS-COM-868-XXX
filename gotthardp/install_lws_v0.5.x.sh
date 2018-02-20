a7f7fdb167d90705f9a7df06e916bc9db6b393c7
Stéphane David <stephane.david@1-gate.com>
2018-02-20 14:29:02 +0100

    Update install_lws_v0.5.x.sh

--- gotthardp/install_lws_v0.5.x.sh
+++ gotthardp/install_lws_v0.5.x.sh
@@ -15,8 +15,8 @@ chmod a+rwx $LWS_DIR
 fi
 
 # Download and install debian package 
-wget https://github.com/gotthardp/lorawan-server/releases/download/v0.5.0/lorawan-server_0.5.0_all.deb
-dpkg -i lorawan-server_0.5.0_all.deb
+wget https://github.com/gotthardp/lorawan-server/releases/download/v0.5.1/lorawan-server_0.5.1_all.deb
+dpkg -i lorawan-server_0.5.1_all.deb
 
 echo "Installation completed."
 
